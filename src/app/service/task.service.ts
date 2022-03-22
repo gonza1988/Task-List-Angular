8e8abdf2fc5ad0bd15e9035e53733e97f9977582
Gonzalo Cozzo <gonza007_88@hotmail.com>
2022-03-22 18:21:06 -0300

    Eliminar y agregar tareas

--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHandler} from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import {TASKS} from '../mock-tasks';
+import { TASKS } from '../mock-tasks';
 import { Task } from '../Task';
+
+const httpOptions = {
+
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +18,23 @@ export class TaskService {
   private apiUrl = 'http://localhost:5001/tasks'
 
   constructor(
-    private http:HttpClient
+    private http: HttpClient
   ) { }
 
-  getTasks(): Observable <Task[]>{
+  getTasks(): Observable<Task[]> {
     return this.http.get<Task[]>(this.apiUrl)
+  }
+  deleteTask(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`
+    return this.http.delete<Task>(url)
+  }
+
+  updtateTaskReminder(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`
+    return this.http.put<Task>(url, task, httpOptions)
+  }
+
+  addTask(task: Task): Observable<Task>{
+    return this.http.post<Task>(this.apiUrl, task, httpOptions)
   }
 }
